447dc203a5841a8d20ae35105582881fbeceacf6
Maksimilijan Marosevic <maksimilijan.marosevic@avl.com>
2022-12-16 11:45:19 +0100

    Day 8 task 1

---
+++ src/day8/task1.rs
@@ -0,0 +1,85 @@
+use std::io::stdin;
+
+#[derive(Debug)]
+struct Cell {
+    height: u8,
+    counted: bool,
+}
+
+pub fn run() {
+    let mut chart = stdin().lines().flatten()
+        .map(|x| x.as_bytes().iter()
+            .map(|y| Cell {height: *y, counted: false})
+            .collect::<Vec<_>>()
+        ).collect::<Vec<_>>();
+
+    println!("{}", count_visible(&mut chart));
+}
+
+fn count_visible(chart: &mut Vec<Vec<Cell>>) -> i32 {
+    let mut count = 0;
+
+    // left - right
+    for line in chart.iter_mut() {
+        let mut highest = 0;
+        for cell in line {
+            if cell.height > highest {
+                highest = cell.height;
+                if !cell.counted {
+                    count += 1;
+                    cell.counted = true
+                }
+            }
+        }
+    }
+
+    // right - left
+    for line in chart.iter_mut() {
+        let mut highest = 0;
+        for cell in line.iter_mut().rev() {
+            if cell.height > highest {
+                highest = cell.height;
+                if !cell.counted {
+                    count += 1;
+                    cell.counted = true
+                }
+            }
+        }
+    }
+
+    let width = chart[0].len();
+
+    println!("{} {}", width, chart.len());
+
+    // up - down
+    for i in 0..width {
+        let mut highest = 0;
+        for j in 0..chart.len() {
+            let mut cell = &mut chart[j][i];
+            if cell.height > highest {
+                highest = cell.height;
+                if !cell.counted {
+                    count += 1;
+                    cell.counted = true
+                }
+            }
+        }
+    }
+
+    // down - up
+    for i in 0..width {
+        let mut highest = 0;
+        for j in (0..chart.len()).rev() {
+            let mut cell = &mut chart[j][i];
+            if cell.height > highest {
+                highest = cell.height;
+                if !cell.counted {
+                    count += 1;
+                    cell.counted = true
+                }
+            }
+        }
+    }
+
+    count
+}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
-mod day6;*/
-mod day7;
+mod day6;
+mod day7;*/
+mod day8;
 
 fn main() {
-    day7::task2::run();
+    day8::task1::run();
 }
